53581aef74eb7bf41830849a2a4c7ee7b07bb22f
Do Cong Hoa <hoadc@vnext.com.vn>
2022-05-10 22:54:27 +0700

    create_migration

--- database/migrations/2022_05_10_144343_create_makers_table.php
+++ database/migrations/2022_05_10_144343_create_makers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateMakersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,13 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('makers', function (Blueprint $table) {
+            $table->id();
+            $table->char('code', 20);
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->softdeletes();
+            $table->timestamps();
         });
     }
 
@@ -27,6 +30,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('makers');
     }
 }
--- database/migrations/2022_05_10_144403_create_products_table.php
+++ database/migrations/2022_05_10_144403_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id(); 
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 100);
+            $table->char('code', 20);
+            $table->float('price', 8, 2);
             $table->string('image')->nullable();
-            $table->unsignedTinyInteger('role')->default('0');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->rememberToken();
+            $table->unsignedBigInteger('maker_id');
+            $table->text('content');
             $table->softdeletes();
             $table->timestamps();
+            $table->foreign('maker_id')->references('id')->on('makers');
         });
     }
 
@@ -34,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 }
